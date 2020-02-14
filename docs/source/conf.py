f978d7f825dc5f0e698c9f49dfb43ff8a13b8f45
Saitama82 <franztrotta@gmail.com>
2020-02-14 16:38:49 +0100

    ok

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,6 +61,9 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
