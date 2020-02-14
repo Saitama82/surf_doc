0e40fdde3ab922c659e71314352a75f2d037015f
Saitama82 <franztrotta@gmail.com>
2020-02-14 16:50:35 +0100

    ok

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -96,6 +96,7 @@ html_theme_options = {
     'headbgcolor': 'white'
 }
 
+html_title = "pySPACE documentation"
 html_logo = "surflogo2.png"
 
 # classic – This is the classic theme, which looks like the Python 2 documentation. It can be customized via these options:
