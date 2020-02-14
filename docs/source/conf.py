8861c073bd174ecda1cf4eb970fdbdca33338bc0
Saitama82 <franztrotta@gmail.com>
2020-02-14 18:38:05 +0100

    ok

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -176,6 +176,14 @@ html_last_updated_fmt = '%b %d, %Y'
 
 
 
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+# html_static_path = ['_static']
+html_static_path = []
+
+
+extensions.append('sphinxcontrib.versioning.sphinx_')
 
 # Custom sidebar templates, filenames relative to this file.
 html_sidebars = {
@@ -185,15 +193,3 @@ html_sidebars = {
  'relations.html',
  'sourcelink.html']
  }
-
-
-
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
-html_static_path = []
-
-
-extensions.append('sphinxcontrib.versioning.sphinx_')
