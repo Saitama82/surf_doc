092904ca32860cb3211b3960d0cc43c5e397e780
Saitama82 <franztrotta@gmail.com>
2020-02-14 13:21:37 +0100

    ok

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -117,7 +117,7 @@ html_theme = 'python_docs_theme'
 #
 html_theme_options = {
     'collapsiblesidebar': True,
-    'issues_url': 'https://docs.python.org/3/bugs.html',
+    # 'issues_url': 'https://docs.python.org/3/bugs.html',
     'root_name':'Surf',
     'root_url':'http://surf.local/',
     'root_icon':'surflogo2.png',
